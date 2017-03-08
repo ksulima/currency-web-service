d4bd981b7ec8e444555df5861a71c9ebb07d2373
Krzysztof Sulima <krzysztof.sulima1991@gmail.com>
2017-03-08 20:40:52 +0100

    first commit

---
+++ demo/demo/src/main/java/com/ksulima/controller/CurrencyExchangeController.java
@@ -0,0 +1,37 @@
+package com.ksulima.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Currency;
+
+/**
+ * Created by Krzysztof Sulima on 08.03.2017.
+ */
+
+
+
+@RestController
+public class CurrencyExchangeController {
+
+
+    @RequestMapping("/multiplier/{value}/{exchangeRatio}")
+    public String currencyMultiplier(@PathVariable Long value,
+                                     @PathVariable Long exchangeRatio,
+                                     @RequestParam("currFrom") String currFrom,
+                                     @RequestParam("currTo") String currTo){
+
+        try{
+            Currency currencyFrom = Currency.getInstance(currFrom);
+        }catch (IllegalArgumentException e){
+            throw new RuntimeException("Something is no yes with @param 'currFrom'");
+        }
+        try{
+            Currency currencyTo = Currency.getInstance(currTo);
+        }catch(IllegalArgumentException e){
+            throw new RuntimeException("Something is no yes with @param 'currTo'");
+        }
+        return value + " " + currFrom + " equals " + value*exchangeRatio + " " + currTo;
+    }
+
+
+}
