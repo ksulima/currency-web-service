da5381f4f60a929c8f655f3979aabdced8f169cd
Krzysztof Sulima <krzysztof.sulima1991@gmail.com>
2017-03-26 18:03:28 +0200

    DB

---
+++ demo/demo/src/main/java/com/ksulima/controller/DatabaseController.java
@@ -0,0 +1,7 @@
+package com.ksulima.controller;
+
+/**
+ * Created by Krzysztof Sulima on 26.03.2017.
+ */
+public class DatabaseController {
+}
---
+++ demo/demo/src/main/java/com/ksulima/database/BookRepository.java
@@ -0,0 +1,7 @@
+package com.ksulima.database;
+
+/**
+ * Created by Krzysztof Sulima on 26.03.2017.
+ */
+public interface Repository {
+}
