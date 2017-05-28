834d8fc4d225ed52019fe0e5a76712377388daf3
Krzysztof Sulima <krzysztof.sulima1991@gmail.com>
2017-05-28 21:04:07 +0200

    clean up unused code

--- demo/demo/src/main/java/com/ksulima/database/repository/CurrencyRepository.java
+++ demo/demo/src/main/java/com/ksulima/database/repository/CurrencyRepository.java
@@ -19,16 +19,5 @@ public interface CurrencyRepository extends JpaRepository<MyCurrency, Long> {
 
     List<MyCurrency> findByDateAndBaseAndWaluta(String date, String base, String waluta);
 
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-=======
-
-//    @Query("select count(u) from  MyCurrency u where u.name = ?1")
-//    Long checkIfdataExist(String name);
->>>>>>> dc675a363da2a34cf5306099dd7008e0f0f30327
-
->>>>>>> c0b8253a0c4977ea974b7407441248be2e24105f
 }
 
